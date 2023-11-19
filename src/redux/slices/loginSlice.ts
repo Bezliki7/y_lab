b4b002a6ff390248b5fee1cd9e9c36999f4c29cf
Bezliki7 <seki4calm@mail.ru>
2023-11-19 17:20:42 +0500

    fetchData

--- src/redux/slices/loginSlice.ts
+++ src/redux/slices/loginSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../store'
+import { useEffect } from "react";
 
 type LoginData = {
     email:string
@@ -8,7 +9,7 @@ type LoginData = {
 }
 
 const initialState = {
-    data: {email: '', password:''} as LoginData
+    data: {email: '', password:'', checkbox:false } as LoginData
 }
 
 
@@ -19,9 +20,20 @@ export const loginSlice = createSlice({
     setLoginData: (state, action) => {
       state.data = action.payload
     },
-
   },
 })
+
+export const FetchData = () => {
+  useEffect(() => {
+    alert('s')
+    fetch("https://mockapi/login", {
+      method: "PUT",
+      body : initialState as any}).then(() => alert('данные отправлны на сервер'))
+  }, [initialState])
+}
+
+
+
 
 export const { setLoginData } = loginSlice.actions
 
